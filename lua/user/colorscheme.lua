c0fc21d2c4fae595e2be8895721e02c1a5fd6b9e
nyxfor13days <harsh1739371@gmail.com>
2022-06-10 10:02:32 +0530

    added colorscheme

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,12 @@
+require('github-theme').setup({
+    theme_style = "dark",
+    function_style = "italic",
+    sidebars = {"qf", "vista_kind", "terminal", "packer"},
+
+    colors = {
+        hint = "orange",
+        error = "#ff0000",
+    },
+
+    transparent = true
+})
