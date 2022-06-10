4ac51e35a0cb290533a547b6bfd1940ac9b404bc
nyxfor13days <harsh1739371@gmail.com>
2022-06-10 10:13:35 +0530

    removed depricated documentation settings in cmp.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -41,6 +41,15 @@ return packer.startup(function(use)
 
   use "projekt0n/github-nvim-theme"
 
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-cmdline"
+  use "saadparwaiz1/cmp_luasnip"
+
+  use "L3MON4D3/LuaSnip"
+  use "rafamadriz/friendly-snippets"
+
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
